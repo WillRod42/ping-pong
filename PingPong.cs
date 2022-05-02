d37e7d7e44fc8e917b5ed2e59b73fddfa327bec5
William Rodriguez <william@rodriguez1.com>
2022-05-02 10:58:16 -0700

    Replace numbers divisible by 3 and 5 in count
    Co-authored-by: Marcus Lorenzo <marcus.kaitara.lorenzo@gmail.com>
    Co-authored-by: Nick Benzinger <nicholasbenzinger@gmail.com>

--- PingPong.cs
+++ PingPong.cs
@@ -4,6 +4,32 @@ public class PingPong
 {
   public static void Main()
   {
-    
+    Console.Write("Enter a number: ");
+    int maxCount = int.Parse(Console.ReadLine());
+    PingPongReplace(maxCount);
+  }
+
+  private static void PingPongReplace(int max)
+  {
+    for (int i = 1; i <= max; i++)
+    {
+      
+      if (i % 3 == 0 && i % 5 == 0)
+      {
+        Console.WriteLine("ping-pong");
+      }
+      else if (i % 3 == 0)
+      {
+        Console.WriteLine("ping");
+      }
+      else if (i % 5 == 0)
+      {
+        Console.WriteLine("pong");
+      }
+      else
+      {
+        Console.WriteLine(i);
+      }
+    }
   }
 }
